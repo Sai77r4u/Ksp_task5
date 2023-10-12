23d737bb3642df98d9675fa7dfbdab849d6de937
Sai77r4u <146876107+Sai77r4u@users.noreply.github.com>
2023-10-12 08:50:33 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,67 @@
+// Hi this is java script
+console.log("Hello saidulu");
+
+// Varibles
+
+let var1=20;
+let var2=50;
+console.log(var2);
+
+let age_of_a_person=60;
+// camel case
+let AgeOfAPerson=60;
+console.log(AgeOfAPerson);
+
+let nameOfPerson="Saidulu";    
+// the above string
+console.log(nameOfPerson);
+
+// constants
+const drivingLicenseAge=18;
+
+console.log(drivingLicenseAge);
+
+let divingLice = 18;
+divingLice =17;
+divingLice = 45;
+console.log(divingLice);
+
+// Operators
+// aurthematical operator -> +,-,*,/,++,--,%    
+// assignment operator -> =,+=,-=,%=,/=,**=
+// comparision operator -> ==,===,<=,>=,!=,<,>
+// logical operator
+
+let number1 = 10;
+let number2 = 20;
+console.log(number1 + number2);
+console.log(number1 - number2);
+console.log(number1 * number2);
+console.log(number1 / number2);
+console.log(number1 % number2);
+
+let num1 = 5;
+let num2 = 2;
+console.log(num1 ** num2); //**means square or power
+
+let age = 23;
+age++; //age +1
+console.log(age);
+
+let apple = 12;
+apple += 23; //apple = apple+23
+console.log(apple);
+
+let number11 = 34;
+let number12 = 43;
+console.log(number11 == number12);
+console.log(number11 === number12);
+console.log(number11 != number12);
+console.log(number11 < number12);
+
+
+
+
+
+
+
